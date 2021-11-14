d5841f193267803809dd3d6f9ae8c2aa321fa8fd
Viktor Persson <vph.persson@gmail.com>
2021-11-14 14:43:48 +0100

    Replace `unwrap`, limit battery level to 100.

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,9 @@
-use std::{fs, thread, time, ffi::CString, ptr};
+use std::{fs, thread, time, ffi::CString, ptr, cmp};
 
 use x11::xlib::{XDefaultScreenOfDisplay, XFlush, XOpenDisplay, XRootWindowOfScreen, XStoreName};
 use chrono::Local;
 
 fn main() {
-
     unsafe {
         let display = XOpenDisplay(ptr::null_mut());
         let screen = XDefaultScreenOfDisplay(display);
@@ -14,7 +13,10 @@ fn main() {
 
         loop {
             let date_str = Local::now().format("%A %Y-%m-%d %H:%M").to_string();
-            let battery_level = fs::read_to_string("/sys/class/power_supply/BAT0/capacity").expect("Battery status could not be read.").trim().parse::<i32>().unwrap();
+            let battery_level = cmp::min(
+                fs::read_to_string("/sys/class/power_supply/BAT0/capacity").expect("Battery status could not be read.").trim().parse::<i32>().expect("Could not parse battery level as an integer."),
+                100
+            );
 
             XStoreName(display, root, CString::new(format!(" {} | {: >3}% ", date_str, battery_level)).unwrap().as_ptr() as *const i8);
             XFlush(display);
